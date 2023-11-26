bbf524876acd0c38d7482b9135efee4993f02fbd
bstachmann <bstachmann@yahoo.de>
2023-11-26 14:42:55 +0000

    Fix: dependencies to get rid of slf4j warnin

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,9 +33,11 @@ allprojects {
     }
 
     dependencies {
-
         implementation(kotlin("script-runtime"))
         implementation(kotlin("stdlib"))
+
+        implementation("org.slf4j:slf4j-api:2.0.9")
+        implementation("org.slf4j:slf4j-simple:2.0.9")    
     }
 
 }
--- generator/build.gradle.kts
+++ generator/build.gradle.kts
@@ -12,10 +12,9 @@ dependencies {
     implementation("io.ktor:ktor-client-cio:$ktorVersion")
     implementation("io.ktor:ktor-client-json:$ktorVersion")
     implementation("io.ktor:ktor-client-jackson:$ktorVersion")
- 
     implementation("org.jetbrains:markdown:0.5.0")
+
     testImplementation(kotlin("reflect"))
     testImplementation("io.kotlintest:kotlintest-runner-junit5:3.1.10")
-    testImplementation("org.slf4j:slf4j-simple:1.7.25")
 }
 
