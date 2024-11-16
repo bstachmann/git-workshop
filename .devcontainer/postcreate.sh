e1fab37002c4c59318641b2bd9fc78428620830c
bstachmann <egal>
2024-11-16 19:15:52 +0000

    update some stuff

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -1,27 +1,27 @@
 sudo ln -s /usr/lib/git-core/git-subtree /usr/local/libexec/git-core/
 
-#git config --global --add safe.directory /workspaces/git-workshop
-git config --global pull.rebase false 
-git config --global merge.conflictStyle diff3 
-git config --global init.defaultBranch main  
-git config --global protocol.file.allow always
+sudo git config --system pull.rebase false 
+sudo git config --system merge.conflictStyle diff3 
+sudo git config --system init.defaultBranch main  
+sudo git config --system protocol.file.allow always
 
+mkdir -p _log
 
 echo "Build exercise files and progress server"
-gradle clean 
-gradle distuebungenMarkdown
+gradle clean distuebungenMarkdown 
 
 
-echo "Run eweb server"
-gradle build run &
+echo "Run web server"
+gradle run &
+
 
 echo run progress server
 gradle progress &
 
-echo "Build website & Webserver on 4000"
-./run-website-build.sh
-./run-server.sh &
 
+echo "Build website & Webserver on 4000"
+./run-website-build.sh 
+./run-server.sh &
 echo "Übungen"
 mkdir -p build/git-uebungen/aufgaben 
 cd build/git-uebungen/aufgaben 
