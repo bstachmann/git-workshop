2f6f08daf266275ad036d5653255cfffd8f5e435
bstachmann <egal>
2024-11-22 15:49:32 +0000

    edit

--- .devcontainer/50-postStart.sh
+++ .devcontainer/50-postStart.sh
@@ -2,9 +2,4 @@
 echo ">>> DEVCONTAINER running $0 in $(pwd) as $(whoami) on $(hostname)" 
 
 
-echo ">>> Starting jekyll server" 
-
-nohup bundle exec jekyll serve -H localhost >_log/server-jekyll.log &
-nohup gradle run >_log/server-workshop-website-jekyll.log &
-nohup gradle progress  >_log/server-progress-monitor.log &
 
