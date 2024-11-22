5c093e35ee1178f6d6b86e8b64fa2f50ce2bb94a
bstachmann <bstachmann@yahoo.de>
2024-11-22 16:38:18 +0000

    log dir

--- scripts/run-all.sh
+++ scripts/run-all.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo ">>> Start all servers for a workshop" 
 
+mkdir -p _log
+
 bundle exec jekyll serve -H localhost 2>&1 >_log/server-jekyll.log &
 gradle run >_log/server-workshop-website-jekyll.log 2>&1 &
 gradle progress  >_log/server-progress-monitor.log 2>&1 &
