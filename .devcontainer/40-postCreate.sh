b1a54e803e35cbfb72f13bf6081f002582a6eb25
bstachmann <egal>
2025-03-01 14:30:02 +0000

    update container creation

--- .devcontainer/40-postCreate.sh
+++ .devcontainer/40-postCreate.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 echo ">>> DEVCONTAINER running $0 in $(pwd) as $(whoami) on $(hostname)" 
 
-echo ">>> Configure git" 
+echo ">>> Configuring Shell prompt"
 
-scripts/git/configure-git-for-workshop.sh
+echo "export PROMPT_DIRTRIM=2" >>/home/vscode/.bashrc
+
 
 echo ">>> Get revealjs sources" 
 
 git submodule update --init revealjs
-
-
-echo ">>> Build generated pages (uebungen) and servers." 
-
-./gradlew build distUebungenMarkdown
-
-./scripts/copy-git-uebungen-to-workspace.sh
-
-echo "Configuring Shell prompt"
-echo "export PROMPT_DIRTRIM=2" >>/home/vscode/.bashrc
