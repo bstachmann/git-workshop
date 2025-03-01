8be01ba30574cd9ddc531ed635347eb4849e855b
bstachmann <egal>
2025-03-01 15:39:54 +0000

    edit

--- scripts/run-webserver-with-progresstracking.sh
+++ scripts/run-webserver-with-progresstracking.sh
@@ -2,6 +2,6 @@
 echo ">>> Start all progress monitor" 
 
 sleep 15 # without that the file /workspaces/git-workshop/build/git-uebungen/aufgaben.json cannot be read. dunno why
-echo $$ >_log/progress-monitor.pid
+echo $$ >_log/webserver-with-progresstracking.pid
 ./gradlew progress 
 
