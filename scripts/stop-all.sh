310a2e3e5779d57447d01a536b8c30d84963a8ee
bstachmann <egal>
2025-03-02 15:35:25 +0000

    fix stop script

--- scripts/stop-all.sh
+++ scripts/stop-all.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo ">>> Stopping all servers for a workshop" 
 
-lsof -ti :8000,4000 | xargs -I {} kill {}
+lsof -i :8000,4000 -sTCP:LISTEN 
 
+lsof -ti :8000,4000 -sTCP:LISTEN | xargs -I {} sh -c "echo killing {}; kill {}"
