f0f7d5637cda72cdf7b726eeaf6373481b178034
bstachmann <egal>
2025-09-14 16:58:02 +0000

    edit

--- generator/src/main/kotlin/de/kapitel26/gitsamplebuilder/sandbox.kt
+++ generator/src/main/kotlin/de/kapitel26/gitsamplebuilder/sandbox.kt
@@ -3,50 +3,85 @@ package de.kapitel26.gitsamplebuilder.gitworkshop
 import de.kapitel26.gitsamplebuilder.createCollectionOfSamples
 import impl.LogBuilderOptions
 import impl.LogOutputFormat
+import kotlin.coroutines.AbstractCoroutineContextElement
+import kotlin.coroutines.CoroutineContext
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+
+class LanguageContext(val language: String) : AbstractCoroutineContextElement(Key) {
+    companion object Key : CoroutineContext.Key<LanguageContext>
+}
 
 fun main() {
     println("This is the sandbox")
 
-    // buildSomeUebungen()
+    buildSomeUebungen()
     // performInLocalBash("ls -lah README.md")
     // openFileInLocalVscode("README.md")
     // openFileInLocalVscode("/root/README.md")
 
-    Infixing().testInfix()
+    // Infixing().testInfix()
 }
 
 class Infixing {
 
     var target_language = "de"
 
-    infix fun String.en(en_text: String): String {
-        if (target_language == "en") return en_text else return this
-    }
-
     fun testInfix() {
-        var result = "Hallo Welt" en "Hello World"
-        println(result)
-        target_language = "en"
-        result = "Hallo Welt" en "Hello World"
-        println(result)
-        target_language = "de"
-        result = "Hallo Welt" en "Hello World"
-        println(result)
+        runBlocking {
+            withContext(LanguageContext("de")) {
+                var result = "Hallo Welt" fake_en "Hello World"
+                println(result)
+            }
+            withContext(LanguageContext("en")) {
+                var result = "Hallo Welt" fake_en "Hello World"
+                println(result)
+            }
+            withContext(LanguageContext("de")) {
+                var result = "Hallo Welt" fake_en "Hello World"
+                println(result)
+            }
+        }
     }
+}
+
+infix suspend fun String.fake_en(en_text: String): String {
+    val lang = kotlin.coroutines.coroutineContext[LanguageContext]?.language ?: "de"
+    return if (lang == "en") en_text else this
+}
+
+object BuildParameters {
+    var target_language = ThreadLocal<String>().apply { set("de") }
+}
+
+infix fun String.en(en_text: String): String {
+    val lang = BuildParameters.target_language.get() ?: "de"
+    return if (lang == "en") en_text else this
 }
 
 fun buildSomeUebungen() {
 
-    createCollectionOfSamples(
-            "gitworkshop-sandbox",
-            LogBuilderOptions(outputFormat = LogOutputFormat.HTML)
-    ) {
-        thema("beispiel") {
+    for (language in listOf("de", "en")) {
+        BuildParameters.target_language.set(language)
+        println("Building samples in language: $language")
+
+        createCollectionOfSamples(
+                "gitworkshop-sandbox $language",
+                LogBuilderOptions(outputFormat = LogOutputFormat.HTML)
+        ) {
+            thema("sandkasten" en "sandbox") {
+                createAufgabenFolge("moin" en ("hello")) {
+                    createIntro("MOIN" en "HELLO", "MOIN MOIN" en "HELLO HELLO")
+                    createAufgabe("Aufgabe" en "Task", "tu was" en "do something") {
+                        markdown("und hier details" en "and here details")
+                    }
+                }
+            }
 
             // halloWelt()
             // rebasingSandbox()
             // repositoryUntersuchen()
-            reverting()
+            // reverting()
             // commitsErstellen()
             // staging()
             // cloning()
