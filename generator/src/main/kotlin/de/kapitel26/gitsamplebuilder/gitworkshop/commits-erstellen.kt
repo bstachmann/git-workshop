ce53d6f28a7cfea0aac976e72899d3c993dd8542
bstachmann <egal>
2025-09-20 15:39:14 +0000

    translate

--- generator/src/main/kotlin/de/kapitel26/gitsamplebuilder/gitworkshop/commits-erstellen.kt
+++ generator/src/main/kotlin/de/kapitel26/gitsamplebuilder/gitworkshop/commits-erstellen.kt
@@ -1,13 +1,13 @@
 package de.kapitel26.gitsamplebuilder.gitworkshop
 
 import impl.CollectionOfSamples
+import impl.en
 
 fun CollectionOfSamples.erstellen() {
 
     createAufgabenFolge("erstellen") {
-
         createIntro(
-                """Commits erstellen""",
+                "Commits erstellen" en "Creating Commits",
                 """
 
                 ## Tipps
@@ -31,6 +31,30 @@ fun CollectionOfSamples.erstellen() {
                 Im Verzeichnis `repo` wartet ein Git-Projekt darauf,
                 bearbeitet zu werden. 
 
+            """ en
+                        """
+
+                ## Tips
+                
+                * `git add <file/directory>`
+                   Before a commit, changes must be registered with `add` in the staging area
+                   (also called the index).
+                * The directory name `.` (which stands for *current directory*) can also be specified.
+                  The command is then applied to all files in the current directory and also subdirectories.
+                * `git commit -m 'My two cents'` Creates a commit with all
+                   changes registered in the staging area.
+                * `git commit -a` Registers all changes to files already versioned in Git
+                  in the staging area, so you can save the separate
+                  `add` call.
+                * The options `-a` and `-m` can also be combined: `git commit -am "Comment"`
+                * `git log --follow -- <file-name>`
+                   Shows the history of a file, even across renames.
+
+                # Setup
+    
+                In the `repo` directory, a Git project is waiting
+                to be edited.
+
             """
         ) {
             createRepo {
@@ -41,13 +65,19 @@ fun CollectionOfSamples.erstellen() {
         }
 
         inRepo {
-
             createAufgabe(
-                    "Commit - mit Staging", """
+                    "Commit - mit Staging" en "Commit - with Staging",
+                    """
                     Bearbeite die Datei `hallo-welt`,
                     füge sie mit `git add` zum Index hinzu (Staging)
                     und erstelle ein Commit mit diesen Änderungen.
-             """) {
+             """ en
+                            """
+                    Edit the file `hallo-welt`,
+                    add it to the index with `git add` (staging)
+                    and create a commit with these changes.
+             """
+            ) {
                 inFile("hallo-welt") { content = "Hallo Welt!" }
                 git("add hallo-welt")
                 git("commit -m 'Erste Änderung'")
@@ -55,51 +85,79 @@ fun CollectionOfSamples.erstellen() {
             }
 
             createAufgabe(
-                    "Commit - automatisches Staging", """
+                    "Commit - automatisches Staging" en "Commit - automatic staging",
+                    """
                     Bearbeite die Datei `hallo-welt` erneut
                     und erstelle wieder ein Commit,
                     dieses mal mal aber mit `-a`.
-             """) {
-
+             """ en
+                            """
+                    Edit the file `hallo-welt` again
+                    and create another commit,
+                    but this time with `-a`.
+             """
+            ) {
                 inFile("hallo-welt") { content = "Hallo Welt!!" }
                 git("commit -am 'Zweite Änderung'")
-                markdown("Mit der Option `-a` kann man sich den `add`-Aufruf sparen:")
+                markdown(
+                        "Mit der Option `-a` kann man sich den `add`-Aufruf sparen:" en
+                                "With the `-a` option, you can save the `add` call:"
+                )
                 git("log --oneline")
             }
 
             createAufgabe(
-                    "Commit - neue Datei", """
+                    "Commit - neue Datei" en "Commit - new file",
+                    """
                     Erstelle `new-world` und bestätige sie mit einem Commit.
-             """) {
-
+             """ en
+                            """
+                    Create `new-world` and confirm it with a commit.
+             """
+            ) {
                 createFile("new-world") { content = "New World!" }
                 git("add new-world")
                 git("commit -m 'Neue Datei'")
             }
 
             createAufgabe(
-                    "Commit - Datei löschen", """
+                    "Commit - Datei löschen" en "Commit - delete file",
+                    """
                     Lösche `hallo-welt` und bestätige dies per Commit.
-             """) {
-
+             """ en
+                            """
+                    Delete `hallo-welt` and confirm this with a commit.
+             """
+            ) {
                 bash("rm hallo-welt")
                 git("commit -am 'Datei löschen'")
             }
 
             createAufgabe(
-                    "⭐ Add - Dateien rekursiv hinzufügen", """
+                    "⭐ Add - Dateien rekursiv hinzufügen" en "⭐ Add - recursively add files",
+                    """
                     Lege eine Datei `superneu` und eine Verzeichnis `sub`mit einer
                     Datei `auchneu` an füge beide mit *einem* Add-Aufruf hinzu und erstelle
                     dann ein Commit.
-             """) {
-                createFile("superneu") 
-                createDir("sub") { 
-                    createFile("auchneu") 
-                }
-                markdown("""
+             """ en
+                            """
+                    Create a file `superneu` and a directory `sub` with a
+                    file `auchneu`, add both with *one* add call and then create
+                    a commit.
+             """
+            ) {
+                createFile("superneu")
+                createDir("sub") { createFile("auchneu") }
+                markdown(
+                        """
                     `.` steht für: *aktuelles Verzeichnis*."
                    Alle Dateien darin und auch darunter werden hinzugefügt.
-                """)
+                """ en
+                                """
+                    `.` stands for: *current directory*."
+                   All files in it and below it will be added.
+                """
+                )
                 git("add .")
                 git("commit -am 'Neue Dateien'")
             }
@@ -107,30 +165,46 @@ fun CollectionOfSamples.erstellen() {
             // dateien erstellen und bearbeiten editor konfigurieren
 
             createAufgabe(
-                    "⭐ Commit - Datei verschieben/umbenennen", """
+                    "⭐ Commit - Datei verschieben/umbenennen" en "⭐ Commit - move/rename file",
+                    """
                     Benenne die Datei `hello-world` in `renamed-world` um
                     und bestätige dies durch ein Commit.
-             """) {
-
+             """ en
+                            """
+                    Rename the file `hello-world` to `renamed-world`
+                    and confirm this with a commit.
+             """
+            ) {
                 bash("mv hello-world renamed-world")
                 git("add renamed-world")
                 git("commit -am 'Umbenennen'")
-                markdown("Anmerkung: Wenn wir `git mv`  statt `mv` genutzt" +
-                        " hätten, dann wäre das separate `git add` nicht nötig gewesen.")
+                markdown(
+                        "Anmerkung: Wenn wir `git mv`  statt `mv` genutzt" +
+                                " hätten, dann wäre das separate `git add` nicht nötig gewesen." en
+                                "Note: If we had used `git mv` instead of `mv`," +
+                                        " the separate `git add` would not have been necessary."
+                )
                 git("log --follow --oneline -- renamed-world")
             }
 
             createAufgabe(
-                    "⭐ Rename detection", """
+                    "⭐ Rename detection",
+                    """
                     Benenne die Datei `datei1` in `datei2` mit `git mv` um. 
                     Sorge dafür, dass die *Rename Detection* dies nicht erkennt.
-             """) {
-
+             """ en
+                            """
+                    Rename the file `datei1` to `datei2` with `git mv`.
+                    Make sure that the *Rename Detection* does not recognize this.
+             """
+            ) {
                 git("mv datei1 datei2")
-                inFile("datei2") { content = "Dieser Inhalt hat nichts mit dem vorigen Inhalt zu tun." }
+                inFile("datei2") {
+                    content = "Dieser Inhalt hat nichts mit dem vorigen Inhalt zu tun."
+                }
                 git("commit -am 'Böse umbenennen'")
                 git("log --follow --oneline -- datei2")
-            }        
+            }
         }
     }
 }
--- generator/src/main/kotlin/de/kapitel26/gitsamplebuilder/gitworkshop/gitmas.kt
+++ generator/src/main/kotlin/de/kapitel26/gitsamplebuilder/gitworkshop/gitmas.kt
@@ -1,14 +1,14 @@
 package de.kapitel26.gitsamplebuilder.gitworkshop
 
 import impl.CollectionOfSamples
+import impl.en
 
 fun CollectionOfSamples.gitmas() {
 
     createAufgabenFolge("gitmas") {
-
         createIntro(
-            """Fröhliche Weihnachten!""",
-            """
+                "Fröhliche Weihnachten!" en "Merry Christmas!",
+                """
                 Auch Git kann ein Weihnachtliches Tool sein.
 
                 Hier geht es darum durch Branchen und mergen 
@@ -23,24 +23,45 @@ fun CollectionOfSamples.gitmas() {
                  * Dann merge den letzten Branch nach ˋmainˋ.
                  * Wenn Du scripten kannst, z.B. mit Bash oder Python: ˋforˋ-Schleifen sind nützlich.
 
+            """ en
+                        """
+                Git can also be a Christmas tool.
+
+                This is about creating a Git graph that looks like a
+                Christmas tree by branching and merging.
+
+                Let your creativity run wild, or follow these **tips:**
+
+                 * Branch a series of branches from ˋmainˋ
+                 * Make a commit on each of the branches so there is something to merge.
+                 * Go to the second branch and merge the first. Then go to the third...
+                 * Then merge the last branch to ˋmainˋ.
+                 * If you can script, e.g. with Bash or Python: ˋforˋ loops are useful.
+
             """
         )
 
         createAufgabe(
-            "Und jetzt ein Weihnachtsbaum", """
+                "Und jetzt ein Weihnachtsbaum" en "And now a Christmas tree",
+                """
                 Branche und Merge so, dass ˋgit log --graph --onelineˋ
                 einen Weihnachtsbaum ausgibt.
+            """ en
+                        """
+                Branch and merge so that ˋgit log --graph --onelineˋ
+                outputs a Christmas tree.
             """
         ) {
-            createRepo("myrepo") { 
+            createRepo("myrepo") {
                 createFileAndCommit("hallo", message = "!")
-                val words = "We wish you a merry Christmas and a happy new year".split(" ").reversed()
-                words.forEachIndexed { i, _  ->
+                val words =
+                        "We wish you a merry Christmas and a happy new year".split(" ").reversed()
+                words.forEachIndexed { i, _ ->
                     git("switch -c b${i} main")
                     createFileAndCommit("f${i}", message = "!")
                 }
                 words.forEachIndexed { i, w ->
-                    val targetBranch = if( i < words.size-1 ) "b${i+1}" else "main"
+                    val targetBranch = if (i < words.size - 1) "b${i+1}" else "main"
                     git("switch ${targetBranch}")
                     git("merge --no-ff b${i} -m '${w}'")
                 }
@@ -48,6 +69,5 @@ fun CollectionOfSamples.gitmas() {
                 git("log --all --oneline --topo-order --graph -${words.size+1} main")
             }
         }
-
     }
 }
