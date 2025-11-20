c0f1430c885245fcb875427df1e7effda1070955
bstachmann <egal>
2025-11-20 17:24:26 +0000

    fix: update SolutionCollector to register tasks by ID and adjust related method calls

--- generator/src/main/kotlin/impl/SolutionCollector.kt
+++ generator/src/main/kotlin/impl/SolutionCollector.kt
@@ -5,8 +5,8 @@ class SolutionCollector {
     val collectedCommands: MutableList<Pair<String, () -> Unit>> = mutableListOf()
     val aufgabenUndSchritte: MutableList<Pair<String, MutableList<String>>> = mutableListOf()
 
-    fun registerAufgabe(title: String) {
-        aufgabenUndSchritte.add(title to mutableListOf<String>())
+    fun registerAufgabe(id:String) {
+        aufgabenUndSchritte.add(id to mutableListOf<String>())
     }
     
     fun registerSchritt(schritt: String) {
